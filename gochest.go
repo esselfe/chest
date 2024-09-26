d303df98f90e5b48fbdcb8e3346649d6381635d3
Stephane Fontaine <esselfe16@gmail.com>
2024-09-26 00:01:33 -0400

    0.2.1: implement the -p option for the Go version

--- gochest.go
+++ gochest.go
@@ -6,106 +6,134 @@ this source inspiring.
 package main
 
 import (
-	"crypto/sha512"
-	"fmt"
-	"io"
-	"os"
-	"strings"
+    "crypto/sha512"
+    "flag"
+    "fmt"
+    "io"
+    "os"
+    "strings"
+    "bufio"
 )
 
 var chest_extension = ".chest"
 
 func hashPassword(password string) []byte {
-	hash := sha512.Sum512([]byte(password))
-	return hash[:]
+    hash := sha512.Sum512([]byte(password))
+    return hash[:]
 }
 
 func processFile(filePath string, password string, mode string) error {
-	hashBytes := hashPassword(password)
-	hashLen := len(hashBytes)
-	hashIndex := 0
+    hashBytes := hashPassword(password)
+    hashLen := len(hashBytes)
+    hashIndex := 0
 
-	var outputFilePath string
-	if mode == "encrypt" {
-		outputFilePath = filePath + chest_extension
-	} else if mode == "decrypt" {
-		if strings.HasSuffix(filePath, chest_extension) {
-			outputFilePath = strings.TrimSuffix(filePath, chest_extension)
-		} else {
-			return fmt.Errorf(
-				"file does not have a %v extension for decryption",
-				chest_extension)
-		}
-	} else {
-		return fmt.Errorf("invalid mode: %s", mode)
-	}
+    var outputFilePath string
+    if mode == "encrypt" {
+        outputFilePath = filePath + chest_extension
+    } else if mode == "decrypt" {
+        if strings.HasSuffix(filePath, chest_extension) {
+            outputFilePath = strings.TrimSuffix(filePath, chest_extension)
+        } else {
+            return fmt.Errorf(
+                "file does not have a %v extension for decryption",
+                chest_extension)
+        }
+    } else {
+        return fmt.Errorf("invalid mode: %s", mode)
+    }
 
-	inputFile, err := os.Open(filePath)
-	if err != nil {
-		return fmt.Errorf("could not open input file: %v", err)
-	}
-	defer inputFile.Close()
+    inputFile, err := os.Open(filePath)
+    if err != nil {
+        return fmt.Errorf("could not open input file: %v", err)
+    }
+    defer inputFile.Close()
 
-	outputFile, err := os.Create(outputFilePath)
-	if err != nil {
-		return fmt.Errorf("could not create output file: %v", err)
-	}
-	defer outputFile.Close()
+    outputFile, err := os.Create(outputFilePath)
+    if err != nil {
+        return fmt.Errorf("could not create output file: %v", err)
+    }
+    defer outputFile.Close()
 
-	buffer := make([]byte, 1)
-	for {
-		_, err := inputFile.Read(buffer)
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			return fmt.Errorf("error reading file: %v", err)
-		}
+    buffer := make([]byte, 1)
+    for {
+        _, err := inputFile.Read(buffer)
+        if err != nil {
+            if err == io.EOF {
+                break
+            }
+            return fmt.Errorf("error reading file: %v", err)
+        }
 
-		byteValue := buffer[0]
-		hashByteValue := hashBytes[hashIndex]
+        byteValue := buffer[0]
+        hashByteValue := hashBytes[hashIndex]
 
-		var outputByteValue byte
-		if mode == "encrypt" {
-			outputByteValue = byte((int(byteValue) - int(hashByteValue)) % 256)
-		} else if mode == "decrypt" {
-			outputByteValue = byte((int(byteValue) + int(hashByteValue)) % 256)
-		}
+        var outputByteValue byte
+        if mode == "encrypt" {
+            outputByteValue = byte((int(byteValue) - int(hashByteValue)) % 256)
+        } else if mode == "decrypt" {
+            outputByteValue = byte((int(byteValue) + int(hashByteValue)) % 256)
+        }
 
-		_, err = outputFile.Write([]byte{outputByteValue})
-		if err != nil {
-			return fmt.Errorf("error writing to output file: %v", err)
-		}
+        _, err = outputFile.Write([]byte{outputByteValue})
+        if err != nil {
+            return fmt.Errorf("error writing to output file: %v", err)
+        }
 
-		// Increment and wrap the hash index
-		hashIndex = (hashIndex + 1) % hashLen
-	}
+        // Increment and wrap the hash index
+        hashIndex = (hashIndex + 1) % hashLen
+    }
 
-	return nil
+    return nil
 }
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: gochest <file>")
-		return
-	}
+    passwordFile := flag.String("p", "", "Password file")
+    flag.Parse()
 
-	filePath := os.Args[1]
-	var password string
-	fmt.Print("Enter the password: ")
-	fmt.Scanln(&password)
+    if flag.NArg() < 1 {
+        fmt.Println("Usage: gochest -p password_file filename")
+        os.Exit(1)
+    }
 
-	// Determine mode based on file extension
-	mode := "encrypt"
-	if strings.HasSuffix(filePath, chest_extension) {
-		mode = "decrypt"
-	}
+    filePath := flag.Arg(0)
 
-	err := processFile(filePath, password, mode)
-	if err != nil {
-		fmt.Println("Error:", err)
-	} else {
-		fmt.Printf("File successfully processed in %s mode.\n", mode)
-	}
+    // Read password from file or prompt the user
+    var password string
+    if *passwordFile != "" {
+        file, err := os.Open(*passwordFile)
+        if err != nil {
+            fmt.Printf("Error: could not open password file %s\n", *passwordFile)
+            os.Exit(1)
+        }
+        defer file.Close()
+
+        scanner := bufio.NewScanner(file)
+        if scanner.Scan() {
+            password = scanner.Text()
+        } else {
+            fmt.Println("Error: password file is empty")
+            os.Exit(1)
+        }
+    } else {
+        fmt.Print("Enter password: ")
+        scanner := bufio.NewScanner(os.Stdin)
+        scanner.Scan()
+        password = scanner.Text()
+    }
+
+    // Determine whether to encrypt or decrypt
+    mode := "encrypt"
+    if strings.HasSuffix(filePath, chest_extension) {
+        mode = "decrypt"
+    }
+
+    // Process the file
+    err := processFile(filePath, password, mode)
+    if err != nil {
+        fmt.Printf("Error: %v\n", err)
+        os.Exit(1)
+    }
+
+    fmt.Printf("%s %s\n", strings.Title(mode), filePath)
 }
 
